4a62e404a5e9ab6bca6cb1c15015f562c9778119
tenglishjr <timothy.english.jr@gmail.com>
2021-11-06 17:27:13 -0500

    Initialize | DeathRace project creation

---
+++ src/main/java/io/github/sandlotminecraft/deathrace/DeathRace.java
@@ -0,0 +1,17 @@
+package io.github.sandlotminecraft.deathrace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathRace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
